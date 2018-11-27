3219990b14d388e3494cd8c55d90ddcf383e69dd
Kiki Pratiwi <kiki.pratiwi.tif17@polban.ac.id>
2018-11-28 04:55:33 +0700

    init project

---
+++ src/Account.java
@@ -0,0 +1,55 @@
+public class Account {
+   private int accountNumber; // account number
+   private int pin; // PIN for authentication
+   private double availableBalance; // funds available for withdrawal
+   private double totalBalance; // funds available & pending deposits
+
+   // Account constructor initializes attributes
+   public Account(int theAccountNumber, int thePIN, 
+      double theAvailableBalance, double theTotalBalance) {
+      accountNumber = theAccountNumber;
+      pin = thePIN;
+      
+      //*keys
+      availableBalance = theAvailableBalance;
+      totalBalance = theTotalBalance; 
+   }
+
+   // determines whether a user-specified PIN matches PIN in Account
+   public boolean validatePIN(int userPIN) {
+      if (userPIN == pin) {
+         return true;
+      }
+      else {
+         //*keys
+         return false;
+      }
+   } 
+
+   // returns available balance
+   public double getAvailableBalance() {
+      return availableBalance;
+   } 
+
+   // returns the total balance
+   public double getTotalBalance() {
+      return totalBalance;
+   } 
+
+   public void credit(double amount) {
+     
+   }
+
+   public void debit(double amount) {
+
+   }
+
+   public int getAccountNumber() {
+      return accountNumber;  
+   }
+   
+   //*keys
+   public int getPin() {
+      return pin;  
+   }   
+} 
---
+++ src/Deposit.java
@@ -0,0 +1,40 @@
+public class Deposit extends Transaction {
+   private double amount; // amount to deposit
+   private Keypad keypad; // reference to keypad
+   private DepositSlot depositSlot; // reference to deposit slot
+   private final static int CANCELED = 0; // constant for cancel option
+
+   // Deposit constructor
+   public Deposit(int userAccountNumber, Screen atmScreen, 
+      BankDatabase atmBankDatabase, Keypad atmKeypad, 
+      DepositSlot atmDepositSlot) {
+
+      // initialize superclass variables
+      super(userAccountNumber, atmScreen, atmBankDatabase);
+
+   } 
+
+   // perform transaction
+   @Override
+   public void execute() {
+ 
+   }
+
+   // prompt user to enter a deposit amount in cents 
+   private double promptForDepositAmount() {
+      Screen screen = getScreen(); // get reference to screen
+
+      // display the prompt
+      screen.displayMessage("\nPlease enter a deposit amount in " + 
+         "CENTS (or 0 to cancel): ");
+      int input = keypad.getInput(); // receive input of deposit amount
+      
+      // check whether the user canceled or entered a valid amount
+      if (input == CANCELED) {
+         return CANCELED;
+      }
+      else {
+         return (double) input / 100; // return dollar amount
+      }
+   }
+} 
