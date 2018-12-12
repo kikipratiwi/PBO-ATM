4e644cb69d9c93607ccfff326da734ac46dbe550
Istamar <istamar.siddiq.tif17@polban.ac.id>
2018-12-12 23:52:36 +0700

    Change PIN Scripts
    These are all script that interfere with change PIN feature.

---
+++ src/ChangePIN.java
@@ -0,0 +1,25 @@
+public class GantiPIN extends Transaction{
+    private Keypad              keypad;                                         // reference to keypad
+    private final static int    CANCELED = 0;                                   // constant for cancel option
+
+    // GantiPIN constructor
+    public GantiPIN(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase, Keypad atmKeypad){
+        // initialize superclass variables
+        super(userAccountNumber, atmScreen, atmBankDatabase);
+        keypad      = atmKeypad;
+    } 
+
+    // perform transaction
+    @Override
+    public void execute(){
+        Screen screen = super.getScreen();
+        screen.displayMessage("Input your new PIN : ");
+        int newPIN = keypad.getInput();
+        if(newPIN == super.getBankDatabase().getLastPIN(super.getAccountNumber())){
+            screen.displayMessageLine("Can't change to your current PIN");
+        }else{
+            super.getBankDatabase().setPIN(super.getAccountNumber(),newPIN);
+            screen.displayMessageLine("\nYou changed your PIN");
+        }
+    }
+}
